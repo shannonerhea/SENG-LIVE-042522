3ee3e51725fc01b34c7cace1b35962884ad6f649
ixnp <enprocopios@gmail.com>
2022-04-26 10:02:33 -0700

    added starter

---
+++ 02-functions/src/index.js
@@ -0,0 +1,6 @@
+//Review 
+//1. Create a constant variable called bookstore with the value 'Easley\'s Technical Books'
+//2. Crate a variable that can be reassigned called open with the value false
+
+
+

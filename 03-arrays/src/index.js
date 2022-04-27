623a26540c6c81f86fa8adc38b87a030518949f4
ixnp <enprocopios@gmail.com>
2022-04-27 09:04:33 -0700

    starter

---
+++ 03-arrays/src/index.js
@@ -0,0 +1,11 @@
+
+const books = [
+    'JavaScript: The Definitive Guide by David Flanagan', 
+    'JavaScript: The Good Parts by Douglas Crockford',  
+    'JavaScript & JQuery: Interactive Front-End Web Development by Jon Duckett', 
+    'Eloquent JavaScript: A Modern Introduction to Programming by Marjin Haverbeke', 
+    'Learn Enough JavaScript to Be Dangerous by Michael Hartl',
+    'Cracking the Coding Interview by Gayle Laakmann McDowell' 
+];
+ 
+const customers =  ['Ceredic Boulderhill', 'Wandrille Farfoot', 'Godobald Goodwort', 'Wibert Bunce', 'Burchard Riverhopper', 'Thietmar Goodwort', 'Adalbert Sandyman','Thierry Oakbottom'];

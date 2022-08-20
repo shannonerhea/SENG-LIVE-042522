3a17841ed1e12d75ba9fa51a8b613c4c551a81a7
shannone <shannone426@gmail.com>
2022-08-20 19:03:06 -0400

    added header footer and card for each book

--- 05-solution/src/index.js
+++ 05-solution/src/index.js
@@ -10,22 +10,56 @@ const renderHeadrer = (store) => {
 
 
 //building out the footer on the page
-//make sure i call this 
+//selectAll cuz there are 3 H5's im gonna use them all below
+//make sure to call this with my other calls ata the bottom of my code 
 const renderFooter = (store) => {
     const footerH5 = document.querySelectorAll('footer h5')
 
     footerH5[0].textContent = store.name // textContent is an atribute that changes the text of the footer 
     footerH5[1].textContent = store.number
     footerH5[2].textContent = store.hours
-    console.log(footerH5)
+    //console.log(footerH5)
 
 };
+
+
+//****adding/creating elements to the DOM*********** */..i need to add li h3 p p img
+const renderInventory = (inventory) => {
+    inventory.forEach((book) => {
+        const card = document.createElement('li')
+        const title = document.createElement('h3')
+        const author = document.createElement('p')
+        const price = document.createElement('p')
+        const img = document.createElement('img')
+
+        title.textContent = book.title
+        author.textContent = book.author
+        price.textContent = book.price
+        img.src = book.imageUrl
+
+        card.className = 'list-li' // ref to CSS styling in CSS file
+        card.append(title, author, price, img) // this append the card to the dom
+        //console.log(card)
+        document.querySelector('#book-list').append(card)
+    });
+    
+
+};
+document.querySelector('#book-list li').remove() // this removed the sample li element
 
 
 
 // all my function calls 
 renderHeadrer(bookStore)
 renderFooter(bookStore)
+renderInventory(bookStore.inventory)
+
+/********NOTES************ */
+// If want to remove something from the DOM (eleminate/remove the node) 
+//document.querySelector('#book-list li').remove()
+//document.querySelector('#book-list li').innerHTML= "" // this is good for empting out an li but not for removing it all togehter
+
+/***************************** */
 
 
 
@@ -49,11 +83,7 @@ renderFooter(bookStore)
 
 
 
-
-
-
-
-
+/****Ixs notes*************************** */
 // const renderHeader = (store) => {
 //     const h1 = document.querySelector("header div h1")
 //     // const header = document.querySelector('header')
